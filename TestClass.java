10ada074e6b204c8aa9531de34ee40d547920a47
phokamofokeng-alt <mofokengkmuzi@gmail.com>
2025-09-17 09:28:37 +0000

    TestClass

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,32 @@ public class TestClass
 {
     public static void main (String [] args)
     {
+        StackAsMyArrayList <String> myStack = new StackAsMyArrayList<>();
+
+        // Print empty list
+        System.out.println(myStack);
+
+        // Populate Stack using push
+        myStack.push(new String ("Godzilla"));
+        myStack.push(new String ("Transporter 2"));
+        myStack.push(new String ("Batman vs Superman"));
+        myStack.push(new String ("The Gentlemen"));
+        System.out.println(myStack);
+
+        // Remove item from Stack. LIFO
+        myStack.pop();
+        System.out.println(myStack);
+
+        // Remove another item
+        myStack.pop();
+        System.out.println(myStack);
+        
+        // Add new item in stack
+        myStack.push(new String ("Bonnie and Clyde"));
+        System.out.println(myStack);
+
+
+
 
     }
 
